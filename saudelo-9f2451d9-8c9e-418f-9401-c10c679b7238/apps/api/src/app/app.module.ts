60c9d837b590359eda6cf0933389eb21e284f882
Suelem Audelo <saudelo@asu.edu>
2026-01-07 23:44:18 -0700

    Setup scaffolding and begin setting up typeorm

---
+++ saudelo-9f2451d9-8c9e-418f-9401-c10c679b7238/apps/api/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'database.sqlite',
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true,
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
